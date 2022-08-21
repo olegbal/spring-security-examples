ade5e28ca36242a13b34812cb35cb73565182e63
Aleh Balakhanau <aleh.balakhanau@solera.com>
2022-08-21 13:48:18 +0400

    Initial commit

---
+++ src/main/java/com/github/olegbal/springsecuritytraining/springsecuritytraining/SpringSecurityTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.github.olegbal.springsecuritytraining.springsecuritytraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/olegbal/springsecuritytraining/springsecuritytraining/SpringSecurityTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.olegbal.springsecuritytraining.springsecuritytraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
